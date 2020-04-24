5b6e6525114a2189b6692dd29431b6a3d895d12f
jsanchez78 <jsanch75@uic.edu>
2020-04-23 20:47:23 -0500

    setAndGet

--- src/main/scala/gitHubObject/gitHubObject.scala
+++ src/main/scala/gitHubObject/gitHubObject.scala
@@ -17,13 +17,13 @@ sealed trait gitHubObject
 object gitHubObject {
   sealed trait HttpUriRequest extends gitHubObject
   sealed trait Client extends gitHubObject
+  sealed trait authCode extends gitHubObject
 
   val BASE_GHQL_URL = "https://api.github.com/graphql"
   val temp = "{viewer {email login url}}"
   val closeable_connection: CloseableHttpClient = HttpClientBuilder.create.build
   val httpUriRequest = new HttpPost(BASE_GHQL_URL)
-  httpUriRequest.addHeader("Authorization", "Bearer 9664303b5648c5c66d68a5b10880dd490c9ac832")
-  httpUriRequest.addHeader("Accept", "application/json")
+
   val gqlReq = new StringEntity("{" +
     "   \"query\":      \"" + client_data.repos + "\", " +
     "   \"operationName\": \"ObtainRepos\",  " +
@@ -31,7 +31,18 @@ object gitHubObject {
     "}" )
   /// gqlReq => function to set any entity based on given (String)
   // Then execute and return json response
-  def setAndGet(str: StringEntity): String = {
+  def setAuthorization(authorizationCode: String): Unit ={
+    ///setAuthorization in builder pattern
+    httpUriRequest.addHeader("Authorization", "Bearer " + authorizationCode)
+    httpUriRequest.addHeader("Accept", "application/json")
+  }
+  def setAndGet(str: String): String = {
+    val gqlReq = new StringEntity("{" +
+      "   \"query\":      \"" + str + "\", " +
+      "   \"operationName\": \"ObtainRepos\",  " +
+      "   \"variables\":  { \"allRepos\": true } " +
+      "}" )
+    httpUriRequest.setEntity(gqlReq)
     val response = closeable_connection.execute(gitHubObject.httpUriRequest)
       response.getEntity match {
       case null => "Response entity is null"
@@ -42,15 +53,14 @@ object gitHubObject {
       }
     }
   }
-  httpUriRequest.setEntity(gqlReq)
+
 
 
   ///Builder hiding complexity but demanding these traits
-  type MandatoryInfo = HttpUriRequest with Client
+  type MandatoryInfo = HttpUriRequest with Client with authCode
 
   def setHeader(accept: Option[String], format: file_format.Value): Unit = {
-
-   httpUriRequest.addHeader("Authorization", "Bearer 9664303b5648c5c66d68a5b10880dd490c9ac832")
+   //httpUriRequest.addHeader("Authorization", "Bearer 9664303b5648c5c66d68a5b10880dd490c9ac832")
     //if (accept.get.compare("Accept") == 1 & format.equals(file_format.APPJSON))
     httpUriRequest.addHeader("Accept", "application/json")
     }
@@ -60,18 +70,17 @@ object gitHubObject {
 }
 
 
-case class gitHubObjectBuilder[I <: gitHubObject](httpUriRequest:Option[HttpPost] = Some(gitHubObject.httpUriRequest),client: Option[CloseableHttpClient] = Some(gitHubObject.closeable_connection)){
+case class gitHubObjectBuilder[I <: gitHubObject](httpUriRequest:Option[HttpPost] = Some(gitHubObject.httpUriRequest),client: Option[CloseableHttpClient] = Some(gitHubObject.closeable_connection),key:String = ""){
   //Default values
   //gitHubObject.setHeader(Some("Accept"),file_format.APPJSON)
-
   //val gqlReq = new StringEntity("{\"query\":\"" + client_data.temp + "\"}" )
-
-
   def withHTTP(httpUriRequest:Option[HttpPost]): gitHubObjectBuilder[I with gitHubObject.HttpUriRequest] =
     this.copy(httpUriRequest = httpUriRequest)
   def withClient(client: Option[CloseableHttpClient]): gitHubObjectBuilder[I with gitHubObject.Client] =
     this.copy(client = client)
+  def withAuthCode(key: String): gitHubObjectBuilder[I with gitHubObject.authCode] =
+    this.copy(key = key)
   def build(implicit ev: I =:= gitHubObject.MandatoryInfo): _gitHubObjectBuilder =
-    _gitHubObjectBuilder(gitHubObject.httpUriRequest,gitHubObject.gqlReq)
+    _gitHubObjectBuilder(gitHubObject.httpUriRequest,gitHubObject.gqlReq,gitHubObject.setAuthorization(key))
 }
-case class _gitHubObjectBuilder(httpUriRequest:HttpPost,gqlReq:StringEntity)
+case class _gitHubObjectBuilder(httpUriRequest:HttpPost,gqlReq:StringEntity,key:Unit)
