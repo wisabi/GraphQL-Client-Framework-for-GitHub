09b99bb08ae9397a8ad1b2473a7f7a2af0cb2a7e
Wisabi <wisam.abunimeh@gmail.com>
2020-04-24 20:00:41 -0500

    added JSON parser class. Input JSON Github query, and outputs a list of repo objects.

---
+++ src/main/scala/RepoParser/JSONParser.scala
@@ -0,0 +1,50 @@
+package RepoParser
+import org.json4s.DefaultFormats
+import org.json4s.jackson.JsonMethods.parse
+
+object JSonParser {
+
+
+  case class R00tJsonObject(data: Data)
+
+  case class Repo(
+                   Owner_and_Repo: String,
+                   Created: String,
+                   Last_Pushed: String,
+                   Description: String,
+                   Disk_Usage: Double,
+                   forks: Forks,
+                   pullRequests: Forks,
+                   LanguagesUsed: LanguagesUsed
+                 )
+
+  case class ListOfRepos(Repo: Repo)
+
+  case class Users_Own_Repos(ListOfRepos: List[ListOfRepos])
+
+  case class Viewer(Users_Own_Repos: Users_Own_Repos, Contributed_To_Repos: Users_Own_Repos)
+
+  case class Data(viewer: Viewer)
+
+  case class Forks(totalCount: Double)
+
+  case class ProgramingLanguage(Language: String)
+
+  case class ListOfLanguages(ProgramingLanguage: ProgramingLanguage)
+
+  case class LanguagesUsed(ListOfLanguages: List[ListOfLanguages])
+
+  //Input JSON string, outputs list of repos.
+  def getRepoList(string: String): List[ListOfRepos] = {
+
+    //Parse JSON
+    implicit val formats = DefaultFormats
+    val jsValue = parse(string)
+
+    //Builds a tree of the JSON and parses it into R00tJsonObject class.
+    val p = jsValue.extract[R00tJsonObject]
+
+    //Gets list of jacob's own repos.
+    p.data.viewer.Users_Own_Repos.ListOfRepos
+  }
+}
