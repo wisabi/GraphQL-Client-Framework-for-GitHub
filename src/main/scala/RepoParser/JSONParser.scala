b71ca72b97a00142db26efed2d7683706d7e0722
Wisabi <wisam.abunimeh@gmail.com>
2020-04-29 15:51:35 -0500

    hmmmm

--- src/main/scala/RepoParser/JSONParser.scala
+++ src/main/scala/RepoParser/JSONParser.scala
@@ -4,36 +4,36 @@ import org.json4s.jackson.JsonMethods.parse
 
 object JSONParser {
 
-
+//
   case class R00tJsonObject(data: Data)
-
-  case class Repo(
-                   Owner_and_Repo: String,
-                   Created: String,
-                   Last_Pushed: String,
-                   Description: String,
-                   Disk_Usage: Double,
-                   forks: Forks,
-                   pullRequests: Forks,
-                   LanguagesUsed: LanguagesUsed
-                 )
-
-  case class ListOfRepos(Repo: Repo)
-
-  case class Users_Own_Repos(ListOfRepos: List[ListOfRepos])
-
-  case class Viewer(Users_Own_Repos: Users_Own_Repos, Contributed_To_Repos: Users_Own_Repos)
-
-  case class Data(viewer: Viewer)
-
-  case class Forks(totalCount: Double)
-
-  case class ProgramingLanguage(Language: String)
-
-  case class ListOfLanguages(ProgramingLanguage: ProgramingLanguage)
-
-  case class LanguagesUsed(ListOfLanguages: List[ListOfLanguages])
-
+//
+//  case class Repo(
+//                   Owner_and_Repo: String,
+//                   Created: String,
+//                   Last_Pushed: String,
+//                   Description: String,
+//                   Disk_Usage: Double,
+//                   forks: Forks,
+//                   pullRequests: Forks,
+//                   LanguagesUsed: LanguagesUsed
+//                 )
+//
+//  case class ListOfRepos(Repo: Repo)
+//
+//  case class Users_Own_Repos(ListOfRepos: List[ListOfRepos])
+//
+//  case class Viewer(Users_Own_Repos: Users_Own_Repos, Contributed_To_Repos: Users_Own_Repos)
+//
+//  case class Data(viewer: Viewer)
+//
+//  case class Forks(totalCount: Double)
+//
+//  case class ProgramingLanguage(Language: String)
+//
+//  case class ListOfLanguages(ProgramingLanguage: ProgramingLanguage)
+//
+//  case class LanguagesUsed(ListOfLanguages: List[ListOfLanguages])
+//
 
 
 
@@ -43,7 +43,8 @@ object JSONParser {
 
   case class CollaboratorsConnection (totalCount: Int,collaborators: Seq[Collaborators])
 
-  case class Data (viewer: Viewer)
+  case class Data (viewer: Viewer, user: User)///////////////////////////
+  //case class Data (user: User)
 
   case class Forks (totalCount: Int)
 
@@ -53,8 +54,8 @@ object JSONParser {
 
   case class PrimaryLanguage (language: String)
 
-  case class PullRequestsConnection (totalCount: Int, pullRequestsList: Seq[])/////////////
-
+  //case class PullRequestsConnection (totalCount: Int, pullRequestsList: Seq[])/////////////
+  //case class ListOfRepos(Repo: Repo)
   case class Repo ( repoName: String,
                     nameWithOwner: String,
                     createdDate: String,
@@ -76,6 +77,20 @@ object JSONParser {
 
   case class Viewer (RepositoriesConnections: RepositoriesConnections)
 
+////////////////////////////
+
+
+  case class Author (userLogin: String)
+
+  //case class Files (path: String)
+
+  //case class FilesConnection (files: Seq[Files])
+
+  case class PullRequestsConnection (totalCount: Int, pullRequestsList: Seq[PullRequestsList])
+
+  case class PullRequestsList (title: String, author: Author,createdDate: String)
+
+  case class User (RepositoriesConnections: RepositoriesConnections)
 
 
 
@@ -83,43 +98,23 @@ object JSONParser {
 
 
 
-
-
-
-  //Input JSON string, outputs list of repos.
-  def getRepoList(string: String): List[ListOfRepos] = {
-
-    //Parse JSON
-    implicit val formats = DefaultFormats
-    val jsValue = parse(string)
-
-    //Builds a tree of the JSON and parses it into R00tJsonObject class.
-    val p = jsValue.extract[R00tJsonObject]
-
-    //Gets list of jacob's own repos.
-    p.data.viewer.Users_Own_Repos.ListOfRepos
-
-
-
-
-  }
-
-  //Input JSON string, outputs list of repos.
-  def getUserOwnRepo(string: String): List[ListOfRepos] = {
-
-    //Parse JSON
-    implicit val formats = DefaultFormats
-    val jsValue = parse(string)
-
-    //Builds a tree of the JSON and parses it into R00tJsonObject class.
-    val p = jsValue.extract[RootInterface]
-
-    //Gets list of jacob's own repos.
-    p.data.viewer.Users_Own_Repos.ListOfRepos
-  }
+//  //Input JSON string, outputs list of repos.
+//  def getRepoList(string: String): Seq[Repo] = {
+//
+//    //Parse JSON
+//    implicit val formats = DefaultFormats
+//    val jsValue = parse(string)
+//
+//    //Builds a tree of the JSON and parses it into R00tJsonObject class.
+//    val p = jsValue.extract[RootInterface]
+//
+//    //Gets list of jacob's own repos.
+//    p.data.viewer.RepositoriesConnections.Repo
+//
+//  }
 
   //Input JSON string, outputs list of repos.
-  def getContributedToRepo(string: String): List[ListOfRepos] = {
+  def getUserOwnRepo(string: String): Seq[Repo] = {
 
     //Parse JSON
     implicit val formats = DefaultFormats
@@ -129,6 +124,19 @@ object JSONParser {
     val p = jsValue.extract[RootInterface]
 
     //Gets list of jacob's own repos.
-    p.data.viewer.Contributed_To_Repos.ListOfRepos
+    p.data.viewer.RepositoriesConnections.Repo
+  }
+
+  //Input JSON string, outputs list of repos.
+  def getSpecificUserRepo(string: String): Seq[Repo]= {
+    //Parse JSON
+    implicit val formats = DefaultFormats
+    val jsValue = parse(string)
+
+    //Builds a tree of the JSON and parses it into R00tJsonObject class.
+    val p = jsValue.extract[RootInterface]
+
+    //Gets list of jacob's own repos.
+    p.data.user.RepositoriesConnections.Repo//.RepositoriesConnections.Repo
   }
 }
